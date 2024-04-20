adf95bfc64ab900baa8a0dcc22f740724611ff5b
Yehor Akunishnikov <46013536+yehor-akunishnikov@users.noreply.github.com>
2024-04-20 20:42:55 +0300

    Cleanup
    Code-style changes
    Cleanup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,20 @@
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-import { type AppType } from "next/app";
-import { Inter } from "next/font/google";
+import {SessionProvider} from 'next-auth/react';
+import {type Session} from 'next-auth';
+import {type AppType} from 'next/app';
 
-import { api } from "~/utils/api";
+import {api} from '~/utils/api';
 
-import "~/styles/globals.css";
-
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+import '~/styles/globals.css';
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
-  pageProps: { session, ...pageProps },
+  pageProps: {session, ...pageProps},
 }) => {
-  return (
-    <SessionProvider session={session}>
-      <main className={`font-sans ${inter.variable}`}>
-        <Component {...pageProps} />
-      </main>
-    </SessionProvider>
-  );
+	return (
+		<SessionProvider session={session}>
+			<Component {...pageProps} />
+		</SessionProvider>
+	);
 };
 
 export default api.withTRPC(MyApp);
