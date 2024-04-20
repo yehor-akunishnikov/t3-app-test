87cd969a15dd1d25cd7b75db977bd67a59687003
Yehor Akunishnikov <46013536+yehor-akunishnikov@users.noreply.github.com>
2024-04-20 21:00:58 +0300

    Updated layout
    Updated layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import {SessionProvider} from 'next-auth/react';
+import {Inter} from 'next/font/google';
 import {type Session} from 'next-auth';
 import {type AppType} from 'next/app';
 
 import {api} from '~/utils/api';
-
 import '~/styles/globals.css';
+
+const inter = Inter({
+	subsets: ['latin'],
+	variable: '--font-sans',
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +17,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
 	return (
 		<SessionProvider session={session}>
-			<Component {...pageProps} />
+			<div className={`font-sans ${inter.variable}`}>
+				<Component {...pageProps} />
+			</div>
 		</SessionProvider>
 	);
 };
